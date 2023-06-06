4fc82ffd892d02191056e16a34983b2be39cfdad
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-06-06 16:02:54 +0200

    refactor setup-git.ps1

--- setup-git.ps1
+++ setup-git.ps1
@@ -2,10 +2,10 @@ $setupGit = Read-Host "Would you like to setup git? (y/n)"
 $setupGit = $setupGit.ToLower()
 
 if ($setupGit -eq 'y') {
-  $userName = Read-Host "Enter your name for git:"
+  $userName = Read-Host "Enter your name for git"
   git config --global user.name $userName
 
-  $userEmail = Read-Host "Enter your email for git:"
+  $userEmail = Read-Host "Enter your email for git"
   git config --global user.email $userEmail
 
   $ignoreCase = Read-Host "Should git ignore case? (y/n)"
