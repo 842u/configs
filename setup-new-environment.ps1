5583244ecd3b491e5a90cda1cb0a6bba71e584bb
Kamil Bazanow <kamil.bazanow@gmail.com>
2023-06-07 15:58:43 +0200

    add chocolatey packages to script

--- setup-new-environment.ps1
+++ setup-new-environment.ps1
@@ -3,6 +3,9 @@ Import-Module "./Install-ChocolateyPackage.psm1"
 # Install/Upgrade Chocolatey
 Invoke-Expression -Command "./install-upgrade-chocolatey.ps1"
 
+# Install/Upgrade PowerShell Core
+Install-ChocolateyPackage -PackageName "powershell-core"
+
 # Install/Upgrade NodeJS
 Install-ChocolateyPackage -PackageName "nodejs"
 
@@ -19,3 +22,33 @@ Invoke-Expression -Command "./setup-gh.ps1"
 
 # Install/Upgrade Oh-My-Posh
 Invoke-Expression -Command "./install-upgrade-oh-my-posh.ps1"
+Invoke-Expression -Command "./setup-oh-my-posh.ps1"
+
+# Install/Upgrade VS Code
+Install-ChocolateyPackage -PackageName "vscode"
+
+# Install/Upgrade VS Code Insiders
+Install-ChocolateyPackage -PackageName "vscode-insiders"
+
+# Install/Upgrage PowerToys
+Install-ChocolateyPackage -PackageName "powertoys"
+
+# Install/Upgrade Google Chrome
+Install-ChocolateyPackage -PackageName "googlechrome"
+
+# Install/Upgrade Firefox
+Install-ChocolateyPackage -PackageName "firefox"
+
+# Install/Upgrade LibreOffice
+Install-ChocolateyPackage -PackageName "libreoffice-fresh"
+
+# Install/Upgrade Blender
+Install-ChocolateyPackage -PackageName "blender"
+
+# Install/Upgrade GIMP
+Install-ChocolateyPackage -PackageName "gimp"
+
+# Install/Upgrade VLC
+Install-ChocolateyPackage -PackageName "vlc"
+
+
